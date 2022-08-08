ea0b668492fceb9a91cdc289c270ea16fe9841bc
ARUF <106890646+Aruf-Hid@users.noreply.github.com>
2022-08-08 12:00:44 +0700

    Update pPvNx.js

--- js/pPvNx.js
+++ js/pPvNx.js
@@ -1,3 +1,125 @@
 /*<![CDATA[*/
-/**Prev Next Post**/var autoPrevNextIndex,showPrevNext;!function(e,t){var a,n={titleLength:"auto",containerId:"pPvNx",newTabLink:!1,callBack:function(){}};for(a in autoPrevNext)n[a]=("undefined"==autoPrevNext[a]?n:autoPrevNext)[a];function r(a){var n=e.createElement("script");n.async="async",n.rel="preload",n.src=a,t.appendChild(n)}function o(e){var t,a,n=e.length;if(0===n)return!1;for(;--n;)t=Math.floor(Math.random()*(n+1)),a=e[n],e[n]=e[t],e[t]=a;return e}var l="object"==typeof psLabels&&0<psLabels.length?"/-/"+o(psLabels)[0]:"";autoPrevNextIndex=function(e){var t=e.feed.openSearch$totalResults.$t-2;e=1,t=0<t?t:1,e=Math.floor(Math.random()*(t-e+1))+e;r("/feeds/posts/summary"+l+"?alt=json-in-script&orderby=updated&start-index="+e+"&max-results=2&callback=showPrevNext")},showPrevNext=function(e){var t,a,r=document.getElementById(n.containerId),l=o(e.feed.entry),s="<div class='pPvNx'>",i=n.newTabLink?' target="_blank"':"";if(r){for(var d=0;d<2&&d!=l.length;d++){a=l[d].title.$t,a="auto"!==n.titleLength&&n.titleLength<a.length?a.substring(0,n.titleLength)+"&hellip;":a;for(var c=0,u=l[d].link.length;c<u;c++)t="alternate"==l[d].link[c].rel?l[d].link[c].href:"#";s+='<a href="'+t+'" '+i+' data-text="'+a+'"></a>'}r.innerHTML=s+="</div>",n.callBack()}},r("/feeds/posts/summary"+l+"?alt=json-in-script&orderby=updated&max-results=0&callback=autoPrevNextIndex")}((window,document),document.getElementsByTagName("head")[0]);
+/**Prev Next Post**/
+! function() {
+
+  var xTabLnk, t = {
+    newTabLink: !0,
+    relOuter: "pPvNx"
+  };
+
+  for (var s in rPstConfig) "undefined" != rPstConfig[s] && (t[s] = rPstConfig[s]);
+
+  function n(a) {
+    var e = document.createElement("script");
+    e.src = a, document.getElementsByTagName("head")[0].appendChild(e)
+  }
+
+  if (xTabLnk = autoPrevNext.newTabLink ? ' target="_blank"' : "", "object" == typeof psLabels && 0 < psLabels.length) var rpLabel = "/-/" + xAR.rdm(psLabels).replace("&amp;", "%26");
+  else rpLabel = "";
+
+
+  arAutoPrevNext = function(a) {
+
+    var p = "",
+      c = getid(t.relOuter),
+      d = a.feed.entry.reverse();
+
+    if (psLabels.includes("_Apps") || psLabels.includes("_Games")) var u = '<div class="pPvNx nApGm">';
+    else u = '<div class="pPvNx">';
+
+    p += u;
+
+    if (2 < d.length) {
+    for (var f = 0; f < d.length; f++)
+      for (var m = 0, h = d[f].link.length; m < h; m++) d[f].link[m].href == t.postUrl && d.splice(f, 1);
+    }
+    
+    if (0 < d.length) {
+      for (var i = 0; i < d.length; i++) {
+        var di = d[i],
+        psTtl = di.title.$t,
+        psTitle = psTtl,
+        psLink,
+        nameApp,
+        verApp,
+        modApp,
+        appVM="",
+        xVh="";
+
+        psLink = gVa(di.link, "rel", "alternate", "href");
+        nameApp = gVa(di.link, "href", "name.app", "type");
+        verApp = gVa(di.link, "href", "ver.app", "type");
+        modApp = gVa(di.link, "href", "mod.app", "type");
+
+        if(nameApp!=""){
+          psTitle=nameApp;
+        }
+
+        if(verApp!=""&&modApp!=""){
+          appVM=' v' + verApp + ' (' + modApp + ")";
+        }else if(verApp!=""&&modApp==""){
+          appVM=' v' + verApp;
+        }
+
+        if(psLink==t.postUrl){
+          xVh = ' class="vh"';
+        }
+
+        if(autoPrevNext.titleLength!="auto" && psTitle.length > autoPrevNext.titleLength){
+          psTitle = psTitle.substring(0, autoPrevNext.titleLength) + "&hellip;";
+        }
+
+        p += '<a'+ xVh +' href="' + psLink + '" ' + xTabLnk + ' data-text="' + psTitle + appVM + '"></a>'
+
+      }
+      c.innerHTML = p += "</div>"
+    }
+
+  }, 
+
+
+  autoPrevNextIndex = function(s) {
+
+    var pEntry = s.feed.entry;
+    var totalResults = s.feed.openSearch$totalResults.$t;
+
+    if(totalResults>1){
+
+      for (var i = 0; i < pEntry.length; i++) {
+        var xCek="";
+
+        var pLink = pEntry[i].link;
+
+          for (var ii = 0; ii < pLink.length; ii++) {
+            if(pLink[ii].rel == "alternate"){
+
+              if(pLink[ii].href == t.postUrl){
+                xCek="OK";
+
+                if(i == 0){
+                  n(t.homePgUrl.replace(/\/$/, "") + "/feeds/posts/summary" + rpLabel + "?alt=json-in-script&orderby=published&start-index=1&max-results=2&callback=arAutoPrevNext");
+                }else{
+                  n(t.homePgUrl.replace(/\/$/, "") + "/feeds/posts/summary" + rpLabel + "?alt=json-in-script&orderby=published&start-index=" + i + "&max-results=3&callback=arAutoPrevNext");
+                }
+
+                break;
+              }
+
+            }
+          }
+
+        if(xCek!=""){break}
+
+      }
+
+    }else{
+      getid(t.relOuter).remove();
+    }
+
+  },
+
+  n(t.homePgUrl.replace(/\/$/, "") + "/feeds/posts/summary" + rpLabel + "?alt=json-in-script&orderby=published&callback=autoPrevNextIndex");
+
+}();
+
 /*]]>*/
