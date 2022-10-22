5897e8b467533ca05dd36b00b3b496a0394d29bb
ARUF <106890646+Aruf-Hid@users.noreply.github.com>
2022-10-22 16:10:59 +0700

    Update fbd.js

--- js/fbd.js
+++ js/fbd.js
@@ -6,32 +6,20 @@ ldJs("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js", "fb-ap", !0, f
   ldJs("https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js", "fb-db", !0, function() {
 
     firebase.initializeApp({
-    	apiKey: ARtb.firebase.key,
-    	databaseURL:ARtb.firebase.db,
-    	
-	 	authDomain: "apmody-by-ar.firebaseapp.com",
-	 	projectId: "apmody-by-ar",
-		storageBucket: "apmody-by-ar.appspot.com",
-		messagingSenderId: "680712225091",
-		appId: "1:680712225091:web:9646b8bad20019cf44eff6"
+		apiKey: ARtb.firebase.apiKey,
+		authDomain: ARtb.firebase.authDomain,
+		databaseURL: ARtb.firebase.databaseURL,
+		projectId: ARtb.firebase.projectId,
+		storageBucket: ARtb.firebase.storageBucket,
+		messagingSenderId: ARtb.firebase.messagingSenderId,
+		appId: ARtb.firebase.appId,
     });
 
     // apc-ARUEF
-    // 768CAC86-5AA3-4658-9E36-E72FC95C50EC
+    // xAR.d("aNzY4Q0FDODYtNUFBMy00NjU4LTlFMzYtRTcyRkM5NUM1MEVD")
+    const appCheck=firebase.appCheck();appCheck.activate(ARtb.firebase.siteKey,!0);
 
-    const appCheck = firebase.appCheck();
-    // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-    // key is the counterpart to the secret key you set in the Firebase console.
-    appCheck.activate(
-      '6Ld6vJ8iAAAAAMPYRjWsWfraGM6Xw2XVvK8DwfRg',
-
-      // Optional argument. If true, the SDK automatically refreshes App Check
-      // tokens as needed.
-      true);
-
-
-
-    var auth = firebase.auth();
+    const auth = firebase.auth();
 
     // Sign Anonym
     auth.signInAnonymously().then(function() {
@@ -42,20 +30,20 @@ ldJs("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js", "fb-ap", !0, f
 
     if(ARtb.firebase.counter.enable){
     	/*read view dl, write view*/
-	    for (var dD = qSell(".pThmb>.iFxd[data-id], .ps.post[data-id]"), dB = firebase.database(), n = 0; n < dD.length; n++) {
-	    	var i = dD[n],
+	    for (let dD = qSell(".pThmb>.iFxd[data-id], .ps.post[data-id]"), dB = firebase.database(), n = 0; n < dD.length; n++) {
+	    	let i = dD[n],
 	    		dC = i.getAttribute("data-id");
 	    	(dC = dB.ref("BlogID_" + blogId + "/" + dC)).once("value", function(a, d) {
 	    		return function(i) {
-	    			var n = i.exists() && null != i.val()._view ? i.val()._view : 0,
+	    			let n = i.exists() && null != i.val()._view ? i.val()._view : 0,
 	    				s = i.exists() && null != i.val()._dl ? i.val()._dl : 0;
 
 	    			if(a.classList.contains("ps")){
-	    				var hC = a.getAttribute("hide-counter");
+	    				let hC = a.getAttribute("hide-counter");
 
 	    				if((n > 0 || s > 0) && (hC==null || (hC!=null && hC!="true"))){
-	    					var eVw = a.querySelector("span[dt-vw]");
-	    					var eDl = a.querySelector("span[dt-dl]");
+	    					let eVw = a.querySelector("span[dt-vw]");
+	    					let eDl = a.querySelector("span[dt-dl]");
 
 	    					if(eVw!=null){
 	    						eVw.setAttribute("dt-vw", xAR.nwc(n));
@@ -67,16 +55,16 @@ ldJs("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js", "fb-ap", !0, f
 	    						eDl.innerText = xAR.nwc(s) + "x";
 	    					}
 
-	    					var lWb = qSel(".arWG > :last-child");
+	    					let lWb = qSel(".arWG > :last-child");
 	    					if(lWb!=null){
-	    						var arVw = '<span data-text="'+ xAR.abv(n) +'" class="wbVw">' + ARtb.firebase.counter.iconView + '</span>';
+	    						let arVw = '<span data-text="'+ xAR.abv(n) +'" class="wbVw">' + ARtb.firebase.counter.iconView + '</span>';
 	    						lWb.insertAdjacentHTML("beforebegin", arVw);
 	    					}
 
 	    				}
 
 	    			}else{
-	    				var hC = a.parentElement.parentElement.getAttribute("hide-counter");
+	    				let hC = a.parentElement.parentElement.getAttribute("hide-counter");
 	    				
 	    				if(!a.classList.contains("s") && (hC==null || (hC!=null && hC!="true")) ){
 	    					
@@ -105,21 +93,21 @@ ldJs("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js", "fb-ap", !0, f
 
 
 	    /*write dl*/
-	    qSell(".ldApGm .cnt > .ld, .bApGm, .btnDL").forEach((a=>{a.addEventListener("click",(l=>{if(!a.classList.contains("o")){a.classList.add("o");var s=firebase.database().ref("BlogID_"+blogId+"/"+postId);s.once("value",(a=>{var l=a.exists()&&null!=a.val()._dl?a.val()._dl:0;l+=1,s.update({_dl:l})}))}}))}));
+	    qSell(".ldApGm .cnt > .ld, .bApGm, .btnDL").forEach((a=>{a.addEventListener("click",(l=>{if(!a.classList.contains("o")){a.classList.add("o");let s=firebase.database().ref("BlogID_"+blogId+"/"+postId);s.once("value",(a=>{let l=a.exists()&&null!=a.val()._dl?a.val()._dl:0;l+=1,s.update({_dl:l})}))}}))}));
 	}
 
 	if(ARtb.firebase.clap.enable){/*CLAP*/
 		if(isIt){
-		if(null!=qSel(".arWG")){var htmlClap='<span class="wbClap" id="arClap">'+ARtb.firebase.clap.icon+"</span>";qSel(".arWG").insertAdjacentHTML("afterbegin",htmlClap)}
-		if(postId)var idps=postId;else idps=pageId;
+		if(null!=qSel(".arWG")){let htmlClap='<span class="wbClap" id="arClap">'+ARtb.firebase.clap.icon+"</span>";qSel(".arWG").insertAdjacentHTML("afterbegin",htmlClap)}
+		if(postId)let idps=postId;else idps=pageId;
 		/*read Clap*/
-		var fDb,eClp=geId("arClap");(fDb=firebase.database().ref("BlogID_"+blogId+"/"+idps)).once("value",function(a,t){return function(t){var e=t.exists()&&null!=t.val()._clap?t.val()._clap:0;e>0&&(a.setAttribute("d-val",e),a.setAttribute("data-text",xAR.abv(e)))}}(eClp));
+		let fDb,eClp=geId("arClap");(fDb=firebase.database().ref("BlogID_"+blogId+"/"+idps)).once("value",function(a,t){return function(t){let e=t.exists()&&null!=t.val()._clap?t.val()._clap:0;e>0&&(a.setAttribute("d-val",e),a.setAttribute("data-text",xAR.abv(e)))}}(eClp));
 		/*LS Clap*/
-		if(null!=xAR.gLS("xUID"))var nmVtLS=xAR.gLS("xUID");else{xAR.sLS("xUID",xUID);nmVtLS=xUID}
+		if(null!=xAR.gLS("xUID"))let nmVtLS=xAR.gLS("xUID");else{xAR.sLS("xUID",xUID);nmVtLS=xUID}
 		const LS_CLP_PS="Vote_"+nmVtLS;let objClpPs={},giClpPs=xAR.gLS(LS_CLP_PS);
 		function synClpPs(s,P,S){switch(s){case"ADD":case"UPD":xAR.gLS(LS_CLP_PS)&&(objClpPs=JSON.parse(xAR.gLS(LS_CLP_PS))),objClpPs[P]=S;break;case"DEL":delete objClpPs[P]}xAR.sLS(LS_CLP_PS,JSON.stringify(objClpPs))}
 		/*write Clap*/
-		geId("arClap").addEventListener("click",(function(){if(!this.classList.contains("a")){this.classList.add("a");var t,a=JSON.parse(xAR.gLS(LS_CLP_PS));parseInt(this.getAttribute("d-val"));if((t=null!=a&&null!=a[idps]?a[idps]:0)<ARtb.firebase.clap.max){var e,i=t+1,s=geId("arClap");(e=firebase.database().ref("BlogID_"+blogId+"/"+idps)).once("value",(l=s,d=e,function(t){var a=t.exists()&&null!=t.val()._clap?t.val()._clap:0;a+=1,d.update({_clap:a}),l.setAttribute("d-val",a),l.setAttribute("data-text",xAR.abv(a))})),synClpPs("ADD",idps,i),toastNotif(ARtb.firebase.clap.txClp+i)}else toastNotif(ARtb.firebase.clap.txMax+t);setTimeout((function(){remCt(geId("arClap"),"a")}),1e3)}var l,d}));
+		geId("arClap").addEventListener("click",(function(){if(!this.classList.contains("a")){this.classList.add("a");let t,a=JSON.parse(xAR.gLS(LS_CLP_PS));parseInt(this.getAttribute("d-val"));if((t=null!=a&&null!=a[idps]?a[idps]:0)<ARtb.firebase.clap.max){let e,i=t+1,s=geId("arClap");(e=firebase.database().ref("BlogID_"+blogId+"/"+idps)).once("value",(l=s,d=e,function(t){let a=t.exists()&&null!=t.val()._clap?t.val()._clap:0;a+=1,d.update({_clap:a}),l.setAttribute("d-val",a),l.setAttribute("data-text",xAR.abv(a))})),synClpPs("ADD",idps,i),toastNotif(ARtb.firebase.clap.txClp+i)}else toastNotif(ARtb.firebase.clap.txMax+t);setTimeout((function(){remCt(geId("arClap"),"a")}),1e3)}let l,d}));
 		}
 	}
 
@@ -142,15 +130,15 @@ ldJs("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js", "fb-ap", !0, f
 if (ARtb.firebase.counter.enable) {
 	function _CvdAG() {
 		dD = qSell(".pThmb>.iFxd[data-id], .ps.post[data-id]");
-		for (var a = firebase.database(), e = 0; e < dD.length; e++) {
-			var t = dD[e],
+		for (let a = firebase.database(), e = 0; e < dD.length; e++) {
+			let t = dD[e],
 				i = t.getAttribute("data-id");
 			(i = a.ref("BlogID_" + blogId + "/" + i)).once("value", function(a, e) {
 				return function(e) {
-					var t = e.exists() && null != e.val()._view ? e.val()._view : 0,
+					let t = e.exists() && null != e.val()._view ? e.val()._view : 0,
 						i = e.exists() && null != e.val()._dl ? e.val()._dl : 0;
 					
-					var hC = a.parentElement.parentElement.getAttribute("hide-counter");
+					let hC = a.parentElement.parentElement.getAttribute("hide-counter");
 					
 					if(!a.classList.contains("s") && (hC==null || (hC!=null && hC!="true")) ){
 						i > 0 && a.insertAdjacentHTML("afterend", '<div class="iFxd dl"><span data-text="' + xAR.abv(i) + '">' + ARtb.firebase.counter.iconDl + "</span></div>");
