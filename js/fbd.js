ae95a404a9c1d38c222e852be811267114be7f91
ARUF <106890646+Aruf-Hid@users.noreply.github.com>
2022-10-24 21:13:29 +0700

    Update fbd.js

--- js/fbd.js
+++ js/fbd.js
@@ -5,154 +5,48 @@ ldJs("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js", "fb-ap", !0, f
   ldJs("https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js", "fb-auth", !0, function() {
   ldJs("https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js", "fb-db", !0, function() {
 
-    firebase.initializeApp({
-		apiKey: ARtb.firebase.apiKey,
-		authDomain: ARtb.firebase.authDomain,
-		databaseURL: ARtb.firebase.databaseURL,
-		projectId: ARtb.firebase.projectId,
-		storageBucket: ARtb.firebase.storageBucket,
-		messagingSenderId: ARtb.firebase.messagingSenderId,
-		appId: ARtb.firebase.appId,
-    });
+    firebase.initializeApp({apiKey:ARtb.firebase.apiKey,authDomain:ARtb.firebase.authDomain,databaseURL:ARtb.firebase.databaseURL,projectId:ARtb.firebase.projectId,storageBucket:ARtb.firebase.storageBucket,messagingSenderId:ARtb.firebase.messagingSenderId,appId:ARtb.firebase.appId});
 
     // apc-ARUEF
     // xAR.d("aNzY4Q0FDODYtNUFBMy00NjU4LTlFMzYtRTcyRkM5NUM1MEVD")
-    const appCheck=firebase.appCheck();appCheck.activate(ARtb.firebase.siteKey,!0);
-
-    const auth = firebase.auth();
+    let appCheck=firebase.appCheck();appCheck.activate(ARtb.firebase.siteKey,!0);let auth=firebase.auth();
 
     // Sign Anonym
-    auth.signInAnonymously().then(function() {
+    auth.signInAnonymously().then(function(){/*begin*/
       console.log('Login Anonym..')
 
     "function"==typeof _dbReady&&_dbReady();
 
-
     if(ARtb.firebase.counter.enable){
     	/*read view dl, write view*/
-	    for (let dD = qSell(".pThmb>.iFxd[data-id], .ps.post[data-id]"), n = 0; n < dD.length; n++) {
-	    	let i = dD[n],
-	    		dC = i.getAttribute("data-id"),
-	    		dB = firebase.database().ref("BlogID_" + blogId + "/" + dC);
-
-	    	dB.once("value", function(a) {
-
-	    		return function(i) {
-	    			let n = i.exists() && null != i.val()._view ? i.val()._view : 0,
-	    				s = i.exists() && null != i.val()._dl ? i.val()._dl : 0;
-
-	    			if(a.classList.contains("ps")){
-	    				let hC = a.getAttribute("hide-counter");
-
-	    				if((n > 0 || s > 0) && (hC==null || (hC!=null && hC!="true"))){
-	    					let eVw = a.querySelector("span[dt-vw]");
-	    					let eDl = a.querySelector("span[dt-dl]");
-
-	    					if(eVw!=null){
-	    						eVw.setAttribute("dt-vw", xAR.nwc(n));
-	    						eVw.innerText = xAR.nwc(n) + "x";
-	    					}
-
-	    					if(eDl!=null){
-	    						eDl.setAttribute("dt-dl", xAR.nwc(s));
-	    						eDl.innerText = xAR.nwc(s) + "x";
-	    					}
-
-	    					let lWb = qSel(".arWG > :last-child");
-	    					if(lWb!=null){
-	    						let arVw = '<span data-text="'+ xAR.abv(n) +'" class="wbVw">' + ARtb.firebase.counter.iconView + '</span>';
-	    						lWb.insertAdjacentHTML("beforebegin", arVw);
-	    					}
-
-	    				}
-
-	    			}else{
-	    				let hC = a.parentElement.parentElement.getAttribute("hide-counter");
-	    				
-	    				if(!a.classList.contains("s") && (hC==null || (hC!=null && hC!="true")) ){
-	    					
-	    					if(s > 0){
-			    				a.insertAdjacentHTML("afterend", '<div class="iFxd dl"><span data-text="' + xAR.abv(s) + '">' + ARtb.firebase.counter.iconDl + "</span></div>");
-	    					}
-
-	    					if(n > 0){
-								a.insertAdjacentHTML("afterend", '<div class="iFxd vw"><span data-text="' + xAR.abv(n) + '">' + ARtb.firebase.counter.iconView + "</span></div>");
-	    					}	    	
-
-							addCt(a, "s")
-
-	    				}
-
-	    			}
-
-	    			"true" == a.getAttribute("data-inc") && (n += 1, dB.update({
-	    				_view: n
-	    			}))
-
-	    		}
-	    	}(i))
-	    }
-
-
+	    for(let t=qSell(".pThmb>.iFxd[data-id], .ps.post[data-id]"),e=0;e<t.length;e++){let n=t[e],a=n.getAttribute("data-id"),l=firebase.database().ref("BlogID_"+blogId+"/"+a);l.once("value",function(t){return function(e){let n=e.exists()&&null!=e.val()._view?e.val()._view:0,a=e.exists()&&null!=e.val()._dl?e.val()._dl:0;if(t.classList.contains("ps")){let e=t.getAttribute("hide-counter");if((n>0||a>0)&&(null==e||null!=e&&"true"!=e)){let e=t.querySelector("span[dt-vw]"),l=t.querySelector("span[dt-dl]");null!=e&&(e.setAttribute("dt-vw",xAR.nwc(n)),e.innerText=xAR.nwc(n)+"x"),null!=l&&(l.setAttribute("dt-dl",xAR.nwc(a)),l.innerText=xAR.nwc(a)+"x");let i=qSel(".arWG > :last-child");if(null!=i){let t='<span data-text="'+xAR.abv(n)+'" class="wbVw">'+ARtb.firebase.counter.iconView+"</span>";i.insertAdjacentHTML("beforebegin",t)}}}else{let e=t.parentElement.parentElement.getAttribute("hide-counter");t.classList.contains("s")||null!=e&&(null==e||"true"==e)||(a>0&&t.insertAdjacentHTML("afterend",'<div class="iFxd dl"><span data-text="'+xAR.abv(a)+'">'+ARtb.firebase.counter.iconDl+"</span></div>"),n>0&&t.insertAdjacentHTML("afterend",'<div class="iFxd vw"><span data-text="'+xAR.abv(n)+'">'+ARtb.firebase.counter.iconView+"</span></div>"),addCt(t,"s"))}"true"==t.getAttribute("data-inc")&&(n+=1,l.update({_view:n}))}}(n))}
 
 	    /*write dl*/
-	    qSell(".ldApGm .cnt > .ld, .bApGm, .btnDL").forEach((a=>{a.addEventListener("click",(l=>{if(!a.classList.contains("o")){a.classList.add("o");let s=firebase.database().ref("BlogID_"+blogId+"/"+postId);s.once("value",(a=>{let l=a.exists()&&null!=a.val()._dl?a.val()._dl:0;l+=1,s.update({_dl:l})}))}}))}));
+	    qSell(".ldApGm .cnt > .ld, .bApGm, .btnDL").forEach((l=>{l.addEventListener("click",(a=>{if(!l.classList.contains("o")){l.classList.add("o");let a=firebase.database().ref("BlogID_"+blogId+"/"+postId);a.once("value",(l=>{let e=l.exists()&&null!=l.val()._dl?l.val()._dl:0;e+=1,a.update({_dl:e})}))}}))}));
 	}
 
 	if(ARtb.firebase.clap.enable){/*CLAP*/
 		if(isIt){
+
 		if(null!=qSel(".arWG")){let htmlClap='<span class="wbClap" id="arClap">'+ARtb.firebase.clap.icon+"</span>";qSel(".arWG").insertAdjacentHTML("afterbegin",htmlClap)}
-		
 		let idps=postId||pageId;
 		/*read Clap*/
 		let fDb,eClp=geId("arClap");(fDb=firebase.database().ref("BlogID_"+blogId+"/"+idps)).once("value",function(a,t){return function(t){let e=t.exists()&&null!=t.val()._clap?t.val()._clap:0;e>0&&(a.setAttribute("d-val",e),a.setAttribute("data-text",xAR.abv(e)))}}(eClp));
 		/*LS Clap*/
-		let nmVtLS;null!=xAR.gLS("xUID")?nmVtLS=xAR.gLS("xUID"):(xAR.sLS("xUID",xUID),nmVtLS=xUID);
-		const LS_CLP_PS="Vote_"+nmVtLS;let objClpPs={},giClpPs=xAR.gLS(LS_CLP_PS);
-		function synClpPs(s,P,S){switch(s){case"ADD":case"UPD":xAR.gLS(LS_CLP_PS)&&(objClpPs=JSON.parse(xAR.gLS(LS_CLP_PS))),objClpPs[P]=S;break;case"DEL":delete objClpPs[P]}xAR.sLS(LS_CLP_PS,JSON.stringify(objClpPs))}
+		let nmVtLS,objClpPs={};
+		null!=xAR.gLS("xUID")?nmVtLS=xAR.gLS("xUID"):(xAR.sLS("xUID",xUID),nmVtLS=xUID);
+		const LS_CLP_PS="Clap_"+nmVtLS,
+		synClpPs=(s,P,S)=>{switch(s){case"ADD":case"UPD":xAR.gLS(LS_CLP_PS)&&(objClpPs=JSON.parse(xAR.gLS(LS_CLP_PS))),objClpPs[P]=S;break;case"DEL":delete objClpPs[P]}xAR.sLS(LS_CLP_PS,JSON.stringify(objClpPs))};
 		
-		/*write Clap*/
-		geId("arClap").addEventListener("click", (function() {
-			if (!this.classList.contains("a")) {
-				this.classList.add("a");
-				let t, a = JSON.parse(xAR.gLS(LS_CLP_PS));
-				parseInt(this.getAttribute("d-val"));
-
-				if ((t = null != a && null != a[idps] ? a[idps] : 0) < ARtb.firebase.clap.max) {
-
-					let dB = firebase.database().ref("BlogID_" + blogId + "/" + idps), 
-						i = t + 1,
-						l = geId("arClap");
-					
-					dB.once("value", (function(t) {
-						let a = t.exists() && null != t.val()._clap ? t.val()._clap : 0;
-						a += 1, dB.update({
-							_clap: a
-						}), l.setAttribute("d-val", a), l.setAttribute("data-text", xAR.abv(a))
-					}));
-
-					synClpPs("ADD", idps, i);
-					toastNotif(ARtb.firebase.clap.txClp + i);
-
-				} else toastNotif(ARtb.firebase.clap.txMax + t);
-
-				setTimeout((function() {
-					remCt(geId("arClap"), "a")
-				}), 1e3);
-			}
-		}));
-
+		/*write Clap*/geId("arClap").addEventListener("click",(function(){if(!this.classList.contains("a")){this.classList.add("a");let t,a=JSON.parse(xAR.gLS(LS_CLP_PS));if(parseInt(this.getAttribute("d-val")),(t=null!=a&&null!=a[idps]?a[idps]:0)<ARtb.firebase.clap.max){let a=firebase.database().ref("BlogID_"+blogId+"/"+idps),e=t+1,s=geId("arClap");a.once("value",(function(t){let e=t.exists()&&null!=t.val()._clap?t.val()._clap:0;e+=1,a.update({_clap:e}),s.setAttribute("d-val",e),s.setAttribute("data-text",xAR.abv(e))})),synClpPs("ADD",idps,e),toastNotif(ARtb.firebase.clap.txClp+e)}else toastNotif(ARtb.firebase.clap.txMax+t);setTimeout((function(){remCt(geId("arClap"),"a")}),1e3)}}));
 
 		}
 	}
 
 
 
-
-    }).catch(function(err) {
-      err.code, 
-      err.message
-    });
+	/*end*/
+    }).catch(function(err){err.code, err.message});
 
 
 
@@ -163,36 +57,7 @@ ldJs("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js", "fb-ap", !0, f
 
 
 /*read view dl*/
-if (ARtb.firebase.counter.enable) {
-	function _CvdAG() {
-		dD = qSell(".pThmb>.iFxd[data-id], .ps.post[data-id]");
-
-		for (let e = 0; e < dD.length; e++) {
-			
-			let dB = firebase.database(),
-				t = dD[e],
-				i = t.getAttribute("data-id");
-
-			dB.ref("BlogID_" + blogId + "/" + i).once("value", function(a) {
-				return function(e) {
-					let t = e.exists() && null != e.val()._view ? e.val()._view : 0,
-						i = e.exists() && null != e.val()._dl ? e.val()._dl : 0;
-					
-					let hC = a.parentElement.parentElement.getAttribute("hide-counter");
-					
-					if(!a.classList.contains("s") && (hC==null || (hC!=null && hC!="true")) ){
-						i > 0 && a.insertAdjacentHTML("afterend", '<div class="iFxd dl"><span data-text="' + xAR.abv(i) + '">' + ARtb.firebase.counter.iconDl + "</span></div>");
-						t > 0 && a.insertAdjacentHTML("afterend", '<div class="iFxd vw"><span data-text="' + xAR.abv(t) + '">' + ARtb.firebase.counter.iconView + "</span></div>");
-						addCt(a, "s")
-					}
-				}
-			}(t))
-		}
-
-	}	
-}
-
-
+if(ARtb.firebase.counter.enable)function _CvdAG(){dD=qSell(".pThmb>.iFxd[data-id], .ps.post[data-id]");for(let e=0;e<dD.length;e++){let t=firebase.database(),a=dD[e],n=a.getAttribute("data-id");t.ref("BlogID_"+blogId+"/"+n).once("value",function(e){return function(t){let a=t.exists()&&null!=t.val()._view?t.val()._view:0,n=t.exists()&&null!=t.val()._dl?t.val()._dl:0,i=e.parentElement.parentElement.getAttribute("hide-counter");e.classList.contains("s")||null!=i&&(null==i||"true"==i)||(n>0&&e.insertAdjacentHTML("afterend",'<div class="iFxd dl"><span data-text="'+xAR.abv(n)+'">'+ARtb.firebase.counter.iconDl+"</span></div>"),a>0&&e.insertAdjacentHTML("afterend",'<div class="iFxd vw"><span data-text="'+xAR.abv(a)+'">'+ARtb.firebase.counter.iconView+"</span></div>"),addCt(e,"s"))}}(a))}}
 
 
 }
