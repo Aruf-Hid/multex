083db6eb9e397543b15c1b9ef4aa69f793e4b9be
ARUF <106890646+Aruf-Hid@users.noreply.github.com>
2023-02-18 02:25:13 +0700

    Update multex-script-1.0.js

--- js/multex-script-1.0.js
+++ js/multex-script-1.0.js
@@ -447,11 +447,13 @@ repText("cmHolder");
 /* Comments url in new tab */ 
 qSell(".cmBd .cmCo a").forEach((t=>{t.setAttribute("target","_blank"),addCt(t,"extL")}));
 
-/*TimeAgo Comment*/
-let dtTm = qSell(".dtTm");
-dtTm.forEach((el)=> {
-  el.innerHTML = timeAgo(Date.parse(el.getAttribute("data-datetime")));
-});
+setTimeout(()=> {
+	/*TimeAgo Comment*/
+	let dtTm = qSell(".dtTm");
+	dtTm.forEach((el)=> {
+	  el.innerHTML = timeAgo(Date.parse(el.getAttribute("data-datetime")));
+	});
+}, 0);
 
 /*lazy yt click thumnail*/
 for(let e=qSell(".lazyYt"),t=0;t<e.length;t++){let a="https://img.youtube.com/vi_webp/"+e[t].dataset.embed+"/sddefault.webp",l=new Image();addCt(l,"lazy"),setAttr(l,"data-src",a),setAttr(l,"src","data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="),setAttr(l,"alt","Youtube video"),l.addEventListener("load",void e[t].appendChild(l)),e[t].addEventListener("click",(function(){let e=document.createElement("iframe");e.setAttribute("frameborder","0"),e.setAttribute("allowfullscreen",""),e.setAttribute("src","https://play.google.com/video/lava/web/player/yt:movie:"+this.dataset.embed+"?autoplay=1&amp;authuser=0&amp;embed=play"),this.innerHTML="",this.appendChild(e)}))}
